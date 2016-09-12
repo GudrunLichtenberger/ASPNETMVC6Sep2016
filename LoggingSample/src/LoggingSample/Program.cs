a3b638c0488e857ac7feb2d8d8b0a9a903ca6cea
Christian Nagel <christian@christiannagel.com>
2016-09-12 15:11:28 +0200

    logging sample

---
+++ LoggingSample/src/LoggingSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoggerFactory factory = new LoggerFactory();
+            var logger = factory.AddConsole().AddDebug().CreateLogger<Program>();
+            logger.LogInformation("info message");
+        }
+    }
+}
