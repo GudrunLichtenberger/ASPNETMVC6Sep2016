01d6468129239a8330d6baa7925f9d3b969ac2be
Christian Nagel <christian@christiannagel.com>
2016-09-14 14:17:12 +0200

    EF 6.x

---
+++ TheReallyOldEF6/TheReallyOldEF6/Book.cs
@@ -0,0 +1,19 @@
+namespace TheReallyOldEF6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Book
+    {
+        public int BookId { get; set; }
+
+        [StringLength(50)]
+        public string Publisher { get; set; }
+
+        [StringLength(40)]
+        public string Title { get; set; }
+    }
+}
